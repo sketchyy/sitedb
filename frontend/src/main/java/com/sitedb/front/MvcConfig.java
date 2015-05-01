7ea20f0bd08d83e385fe69c1203fd1720664f17e
Alexander <alexansavon@gmail.com>
2015-05-01 15:19:52 +0300

    Added authentication, cookies, roles, and register stub

--- frontend/src/main/java/com/sitedb/front/MvcConfig.java
+++ frontend/src/main/java/com/sitedb/front/MvcConfig.java
@@ -11,12 +11,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @Configuration
 public class MvcConfig extends WebMvcConfigurerAdapter {
 
+
+
+
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/home").setViewName("home");
         registry.addViewController("/").setViewName("home");
         registry.addViewController("/hello").setViewName("hello");
-        registry.addViewController("/login").setViewName("login");
+       // registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/error").setViewName("error");
     }
 
 
